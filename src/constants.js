d6e057b1305ecfb3988f961a348cddfbfc358e4f
Adil Mustafa <dev.adil786@gmail.com>
2024-12-04 03:06:38 +0500

    initial commit

---
+++ src/constants.js
@@ -0,0 +1,81 @@
+export const techStack = [
+
+
+  { name: "TypeScript", link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg" },
+  { name: "Express", link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg" },
+
+  { name: "NodeJS", link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" },
+  { name: "Postman", link: "https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg" },
+
+  { name: "React Native", link: "https://pagepro.co/blog/wp-content/uploads/2020/03/react-native-logo-884x1024.png" },
+  { name: "HTML", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" },
+  { name: "CSS", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg" },
+  { name: "Bootstrap", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain.svg" },
+  { name: "Redux", link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg" },
+  { name: "Sass", link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg" },
+
+  { name: "Javascript", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" },
+  { name: "Tailwind CSS", link: "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg" },
+  { name: "React", link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" },
+  { name: "Mongo DB", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-plain.svg" },
+  // { name: "Azure", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/azure/azure-plain.svg" },
+  { name: "Git", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain.svg" },
+  // { name: "Docker", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-plain.svg" },
+  { name: "Firebase", link: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg" },
+  { name: "VS Code", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg" },
+  { name: "MobX", link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mobx/mobx-original.svg" },
+  { name: "RTK", link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg" },
+  { name: "Ant Design", link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/antd/antd-original.svg" },
+  { name: "Material UI", link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/materialui/materialui-original.svg" },
+  { name: "GitLab", link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/gitlab/gitlab-original.svg" },
+  { name: "AWS", link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/aws/aws-original.svg" },
+  { name: "DigitalOcean", link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/digitalocean/digitalocean-original.svg" },
+  { name: "KafkaJS", link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/kafka/kafka-original.svg" }
+
+  // { name: "Bash", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bash/bash-original.svg" },
+  // { name: "Vim", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vim/vim-original.svg" },
+
+  // {name: "", link: "https://repository-images.githubusercontent.com/59065830/b62be480-45d2-11ea-9989-803db0f9c44d"}
+]
+
+
+export const contactLinks = [{ name: 'Stack Overflow', url: 'https://img.icons8.com/external-tal-revivo-color-tal-revivo/40/000000/external-stack-overflow-is-a-question-and-answer-site-for-professional-logo-color-tal-revivo.png', link: 'https://stackoverflow.com/users/11788531/adil-sharma' },
+{ name: 'Instagram', url: 'https://img.icons8.com/doodle/40/000000/instagram-new--v2.png', link: 'https://www.instagram.com/adilsh1999/' },
+{ name: 'Github', url: 'https://img.icons8.com/doodle/40/000000/github--v1.png', link: 'https://github.com/Adil7767/Adil7767/blob/main/README.md' },
+{ name: 'LinkedIn', url: 'https://img.icons8.com/doodle/40/000000/linkedin--v2.png', link: 'https://www.linkedin.com/in/adilmustafa7767' }
+]
+
+
+
+export const serviceData = [
+  {
+    name: 'Web Application Development',
+    desc: 'Building modern, responsive websites with React and Node.js to deliver seamless user experiences.',
+    img: "https://via.placeholder.com/64/0000FF/808080?Text=Web+Dev"
+  },
+  {
+    name: 'Mobile Application Development',
+    desc: 'Creating cross-platform mobile apps with React Native, ensuring fast performance and intuitive interfaces.',
+    img: "https://via.placeholder.com/64/008000/FFFFFF?Text=Mobile+App"
+  },
+  {
+    name: 'Backend Development',
+    desc: 'Developing scalable backend systems with Node.js, Express, and databases to power your applications.',
+    img: "https://via.placeholder.com/64/FF5733/FFFFFF?Text=Backend+Dev"
+  },
+  {
+    name: 'UI/UX Design',
+    desc: 'Designing clean, user-friendly interfaces that enhance usability and ensure a seamless experience.',
+    img: "https://via.placeholder.com/64/FFD700/FFFFFF?Text=UI+UX+Design"
+  },
+  {
+    name: 'SEO & Performance Optimization',
+    desc: 'Optimizing web performance and SEO to improve speed, rankings, and user engagement.',
+    img: "https://via.placeholder.com/64/800080/FFFFFF?Text=SEO+Optimization"
+  },
+  {
+    name: 'Cloud & API Integrations',
+    desc: 'Integrating APIs and deploying cloud solutions to scale and extend application functionality.',
+    img: "https://via.placeholder.com/64/FF6347/FFFFFF?Text=Cloud+API"
+  }
+];
